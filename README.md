5e80530664018d571abea6a38c04b17fb1e2961b
cosrayjamie <jamie.zvirzdin@gmail.com>
2019-03-30 18:51:59 -0400

    Deleted extra lines in guessinggame.sh

--- README.md
+++ README.md
@@ -4,8 +4,8 @@
 
 ### Date & time at which make was run: 
 
-03/30/2019,06:45:50 PM
+03/30/2019,06:51:20 PM
 
 ### The number of lines of code contained in guessinggame.sh: 
 
-35 
+29 
--- guessinggame.sh
+++ guessinggame.sh
@@ -1,18 +1,13 @@
-#!/usr/bin/env Bash
-# File: guessinggame.sh
-
 function getfilenum { 
 	for filenums in $(find . \( ! -regex '.*/\..*' \) -type f | wc -l) #excludes hidden files
 	do
 		echo $filenums #returns number of files in directory
 	done
 }
-
 function guessinggame {
 	local fsum=$(getfilenum $@)
 	echo "Welcome to the Guessing Game. Guess how many files are in the current directory."
 	read guess
-
 	while [[ $guess != $fsum ]] #Loops if guess is wrong.
 	do
 		if [[ $guess -gt $fsum ]]
@@ -25,7 +20,6 @@ function guessinggame {
 			read guess
 		fi
 	done
-
 	if [[ $guess =~ $fsum ]] #If guess is correct, program ends.
 	then
 		echo "Congratulations, you guessed correctly. Program complete."
